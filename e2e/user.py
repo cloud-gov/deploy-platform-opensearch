a8af4cfeaf80c6c1f338fb916d751104082ad322
Jason <jason.gambino@gsa.gov>
2026-04-28 12:41:13 -0400

    removing old things from logs and adding in staging jobs

--- e2e/user.py
+++
@@ -1,9 +0,0 @@
-import pyotp
-
-
-class User:
-    def __init__(self, username, password, totp_seed):
-        self.username = username
-        self.password = password
-        self.totp_seed = totp_seed
-        self.totp = pyotp.TOTP(totp_seed)
--- scripts/e2e.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements-test.txt
-playwright install firefox
-python -m pytest e2e --browser firefox --tracing retain-on-failure "$@"
